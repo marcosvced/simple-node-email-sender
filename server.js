8242b70b50c5c43c4c1911814680fe66cf0fda3b
Marcos Vicente Espasandín <marcosvicenteespasandin@gmail.com>
2020-11-29 16:57:04 +0100

    fix body parse deprecate

--- server.js
+++ server.js
@@ -1,50 +1,48 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser')
 const nodemailer = require('nodemailer');
 
 const app = express();
 const path = require('path');
 const PORT = 8080;
 const TRANSPORTER = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL_FROM,
-      pass: process.env.EMAIL_PASS
-    }
-  });
-
-// Configuring our data parsing
-app.use(express.urlencoded({
-    extend: false
-}));
-app.use(express.json());
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '.', 'index.html'));
-    console.log(process.env.EMAIL_FROM)
-    //__dirname : It will resolve to your project folder.
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_FROM,
+    pass: process.env.EMAIL_PASS
+  }
 });
 
-app.post('/email', (req, res) => {
-    //Send an email here but currently dummy email
-    console.log('Data:', req.body.data);
-    res.json({message: 'Message received!'})
+const jsonParser = bodyParser.json()
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  console.log(process.env.EMAIL_FROM)
+  //__dirname : It will resolve to your project folder.
+});
+
+app.post('/email', urlencodedParser, (req, res) => {
+  //Send an email here but currently dummy email
+  console.log('Data:', req.body);
+  res.json({ message: 'Message received!' })
 
 
-      const email = {
-        from: process.env.EMAIL_FROM,
-        to: process.env.EMAIL_TO,
-        subject: `From: ${req.body.email}`,
-        text: req.body.text
-      };
-      
-      TRANSPORTER.sendMail(email, (error, info) => {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-      });
+  const email = {
+    from: process.env.EMAIL_FROM,
+    to: process.env.EMAIL_TO,
+    subject: `From: ${req.body.email}`,
+    text: req.body.text
+  };
+
+  TRANSPORTER.sendMail(email, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
 
 });
 
